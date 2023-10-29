26854f4521fcc738428d26b3003564d1d23bd3cd
Talha Sayyed <talhasayyed69@gmail.com>
2023-10-30 03:54:10 +0530

    small ui changes

--- script.js
+++ script.js
@@ -38,14 +38,17 @@ function updateCategoryDropdown() {
 
 // Function to update the Product list
 function updateProductList() {
-    const productsList = document.getElementById('products');
-    productsList.innerHTML = '';
+    const productsTable = document.getElementById('productsTable');
+    productsTable.innerHTML = '';
+
     for (const country in filteredData) {
         for (const category in filteredData[country]) {
             for (const product of filteredData[country][category]) {
-                const listItem = document.createElement('li');
-                listItem.textContent = product;
-                productsList.appendChild(listItem);
+                productsTable.innerHTML += `
+                    <tr>
+                        <td>${product}</td>
+                    </tr>
+                `;
             }
         }
     }
@@ -92,22 +95,107 @@ document.getElementById('productSearch').addEventListener('input', () => {
     const searchText = document.getElementById('productSearch').value.toLowerCase();
     const newFilteredData = {};
 
-    for (const country in filteredData) {
-        for (const category in filteredData[country]) {
-            for (const product of filteredData[country][category]) {
-                if (product.toLowerCase().includes(searchText)) {
-                    if (!newFilteredData[country]) {
-                        newFilteredData[country] = {};
+    if (searchText === '') {
+        // If the search input is empty, restore the initial or filtered data
+        filteredData = data; // Set back to the original data or filtered data
+        updateProductList();
+    } else {
+        for (const country in filteredData) {
+            for (const category in filteredData[country]) {
+                for (const product of filteredData[country][category]) {
+                    if (product.toLowerCase().includes(searchText)) {
+                        if (!newFilteredData[country]) {
+                            newFilteredData[country] = {};
+                        }
+                        if (!newFilteredData[country][category]) {
+                            newFilteredData[country][category] = [];
+                        }
+                        newFilteredData[country][category].push(product);
                     }
-                    if (!newFilteredData[country][category]) {
-                        newFilteredData[country][category] = [];
-                    }
-                    newFilteredData[country][category].push(product);
                 }
             }
+        }
+
+        filteredData = newFilteredData;
+        updateProductList();
+    }
+});
+
+
+// Rest of the code before
+
+// Function to sort products alphabetically
+function sortProductsAlphabetically() {
+    for (const country in filteredData) {
+        for (const category in filteredData[country]) {
+            filteredData[country][category].sort();
+        }
+    }
+}
+
+// Function to generate filter buttons for product names
+function generateFilterButtons() {
+    const filterButtonsContainer = document.getElementById('filterButtons');
+    filterButtonsContainer.innerHTML = '';
+
+    for (let charCode = 65; charCode <= 90; charCode++) {
+        const letter = String.fromCharCode(charCode);
+        const filterButton = document.createElement('button');
+        filterButton.className = 'btn btn-primary';
+        filterButton.textContent = letter;
+        filterButton.addEventListener('click', () => {
+            // clearSearchInput(); // Clear the search input
+            filterProductsByLetter(letter); // Apply the alphabet filter
+        });
+        filterButtonsContainer.appendChild(filterButton);
+    }
+}
+
+// // Function to clear the search input
+// function clearSearchInput() {
+//     document.getElementById('productSearch').value = '';
+// }
+
+
+// Function to filter products by starting letter
+function filterProductsByLetter(letter) {
+    const newFilteredData = {};
+
+    for (const country in filteredData) {
+        newFilteredData[country] = newFilteredData[country] || {};
+
+        for (const category in filteredData[country]) {
+            newFilteredData[country][category] = filteredData[country][category].filter(product =>
+                product.toLowerCase().startsWith(letter.toLowerCase())
+            );
         }
     }
 
     filteredData = newFilteredData;
     updateProductList();
-});
+}
+
+
+// Function to display all products in a Bootstrap table
+function displayAllProducts() {
+    const productsTable = document.getElementById('productsTable');
+    productsTable.innerHTML = '';
+
+    for (const country in filteredData) {
+        for (const category in filteredData[country]) {
+            for (const product of filteredData[country][category]) {
+                productsTable.innerHTML += `
+                    <tr>
+                        <td>${product}</td>
+                    </tr>
+                `;
+            }
+        }
+    }
+}
+
+// Sort and display products alphabetically
+sortProductsAlphabetically();
+generateFilterButtons();
+displayAllProducts();
+
